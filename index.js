eab12d9a2f329f3d8edcda9f99e43e7724c47a7c
Arthur Escalera <arthur@tideal.com.br>
2022-08-10 11:53:30 -0300

    Add getProp

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-import { Search, DatabaseId } from "./modules.js";
+import { Search, DatabaseId, Page } from "./modules.js";
 
 
 // Search(DatabaseId.string())
 //   .then((r) => {
-//   console.log(r.results[0].properties)
-//   console.log(r.results[0].properties['Tags'].multi_select, "TAGS 🉐")
+//   console.log(r.results, '👽')
+//   // console.log(r.results[0].properties['Tags'].multi_select, "TAGS 🉐")
 // })
 
 
 Search('Yurts')
   .then((r) => {
-    console.log(r.results[1], 'É a page 💠📃 ______------__---__-')
-  console.log(r.results[1].properties['Tags'], 'É a page 📃')
+    console.log(r.results[1], 'É a page 💠')
+    // console.log(r.results[1].properties['Tags'], 'É a page 📃')
 })
+
+// Page.getProp('2905897a664e4586b3263070572b1b3f', 'fddZ')
+//   .then((r) => {
+//   console.log(r, 'tags da pagina tal 🤩')
+// })
--- src/Controller/Page.js
+++ src/Controller/Page.js
@@ -9,27 +9,37 @@ import { NotionClient, DatabaseId } from "../../data.js";
 
 export default class Page {
 
-    static async add (text) {
-      try {
-        const response = await NotionClient.pages.create({
-          parent: { database_id: DatabaseId },
-          properties: {
-            title: {
-              title: [
-                {
-                  "text": {
-                    "content": text
-                  }
+  static async add(text) {
+    try {
+      const response = await NotionClient.pages.create({
+        parent: { database_id: DatabaseId },
+        properties: {
+          title: {
+            title: [
+              {
+                "text": {
+                  "content": text
                 }
-              ]
-            }
-          },
-        });
-        console.log(response);
-        return response;
-      } catch (error) {
-        return error;
-      }
-    };
+              }
+            ]
+          }
+        },
+      });
+      console.log(response);
+      return response;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  static async getProp(pageId, propId) {
+    try {
+      const response = await NotionClient.pages.properties.retrieve({ page_id: pageId, property_id: propId });
+      console.log(response, '🦎')
+      return response
+    } catch (error) {
+      return error
+    }
+  }
 
 }
